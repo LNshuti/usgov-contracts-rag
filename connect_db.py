820ce66e1482e226f8603c145d9de990a4800bdc
Leonce Nshuti <nshutl0@sewanee.edu>
2024-01-07 19:04:56 -0600

    use proper table name

--- connect_db.py
+++ connect_db.py
@@ -7,7 +7,7 @@ XLSX_FILE_PATH = 'data/BuyAmericanActExceptionsandWaiversOctober2023.xlsx'
 CSV_FILE_PATH = 'data/contract_subset.csv'
 PARQUET_FILE_PATH = 'data/sample_contract_df.parquet'
 DB_FILE_PATH = 'gov-contracts.db'
-TABLE_NAME = 'table_name'
+TABLE_NAME = 'BuyAmericanActExceptionsandWaiversOct2023'
 
 
 def remove_columns_with_missing_data(df, threshold=0.05):
